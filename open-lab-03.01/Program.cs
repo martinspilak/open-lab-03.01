9094fef72a66a57066a169d1445521f523a1f54d
martinspilak <martinecko24@gmail.com>
2019-12-06 21:31:00 +0100

    open-lab-03.01

---
+++ open-lab-03.01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace open_lab_03._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array2D = new string[,] {{ "one", "two", "three"}, {"Apple", "orange", "banana"}, };
+            Console.WriteLine(array2D[1, 2]);
+        }
+    }
+}
